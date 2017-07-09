f6d6aa00e452684a95d633f8cefdb92fc3edbf37
Vova <Vladimir59912@gmail.com>
2017-07-09 04:44:19 +0300

    Отобразил дату

--- static/components/ChannelPage.js
+++ static/components/ChannelPage.js
@@ -1,9 +1,11 @@
 class Post extends React.Component {
   render() {
     const {sentDate, text} = this.props.post
+    const date = moment.unix(sentDate).format('YYYY-MM-DD HH:mm');
     return <div className="panel">
-      <b>{sentDate}</b>
-      {text}
+      <b>{date}</b>
+      <p>{text}</p>
+       <hr/>
     </div>
   }
 }
