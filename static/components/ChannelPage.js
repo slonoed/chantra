5290cb12f4e20dbcdfe80b83ca39ad56b9802798
Vova <Vladimir59912@gmail.com>
2017-07-22 22:09:55 +0300

    отобразил посты при отправке

--- static/components/ChannelPage.js
+++ static/components/ChannelPage.js
@@ -1,20 +1,17 @@
-class Post extends React.Component {
-  render() {
-    const {sentDate, text} = this.props.post
-    const date = moment.unix(sentDate).format('YYYY-MM-DD HH:mm');
-    return <div className="panel">
-      <b>{date}</b>
-      <p>{text}</p>
-       <hr/>
-    </div>
-  }
-}
+
 class ChannelPage extends React.Component {
+  constructor(props) { 
+    super(props); 
+    const {posts} = INITIAL_DATA
+    this.state = {posts};
+  }
   onMessageAdd(message) {
+   const {posts} = this.state
     const content = Object.assign(message, {
       sentDate: message.sentDate.unix(),
       channel_id: this.props.channelId
-    });
+    
+    })
     fetch('/api/addPost', {
       method: 'POST',
       body: JSON.stringify(content),
@@ -24,28 +21,36 @@ class ChannelPage extends React.Component {
     .then(r => {
       if (!r.ok) {
         throw new Error(r.error || 'Unknown error');
+      } 
+     else {
+      const newPosts = posts.concat(r.post);
+      this.setState({ posts: newPosts });
+      {window.alert("сообщение отправлено")};
       }
     })
     .catch(e => {
       alert(e.toString());
-    });
+    }); 
   }
+
+ 
   render() {
-    const {channels, bots, posts} = INITIAL_DATA
+    const {channels, bots} = INITIAL_DATA
     const {channelId} = this.props;
     const channel = channels.find(c => c.id === channelId)
-    const {title} = channel
-    const filteredPosts = posts.filter(p => p.channel_id === channelId)
+    const {posts} = this.state
+    const filteredPosts = this.state.posts.filter(p => p.channel_id === channelId)
     const byDate = (b, a) => a.sentDate - b.sentDate;
-    return <div>
-      <h2>{title}</h2>
-      <MessageForm
+    const {title} = channel
+      return <div>
+        <h2>{title}</h2>
+        <MessageForm
         bots={bots}
         onAdd={m => this.onMessageAdd(m)}
       />
-      <hr/>
+     <hr/>
       <h2>Posts</h2>
-      {filteredPosts.sort(byDate).map(p => <Post post={p}/>)}
+        {filteredPosts.sort(byDate).map(p => <PostBlock post={p}/>)}
     </div>
   }
 }
---
+++ static/components/PostBlock.js
@@ -0,0 +1,11 @@
+class PostBlock extends React.Component {
+render() {
+ const {sentDate, text} = this.props.post
+ const date = moment.unix(sentDate).format('YYYY-MM-DD HH:mm');
+ return <div className="panel">
+   <b>{date}</b>
+   <p>{text}</p>
+      <hr/>
+   </div>
+  }
+}
