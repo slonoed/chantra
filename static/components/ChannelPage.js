a376f363aa0bcc82cdaae9e1068b57dc95a7a42e
Vova <Vladimir59912@gmail.com>
2017-07-23 00:14:55 +0300

    Вменяемый Алерт

--- static/components/ChannelPage.js
+++ static/components/ChannelPage.js
@@ -1,10 +1,16 @@
+class Alert extends React.Component {
+  render() {
+    return <h1>Done!</h1>
+  }
+}
 
 class ChannelPage extends React.Component {
   constructor(props) { 
     super(props); 
     const {posts} = INITIAL_DATA
-    this.state = {posts};
+    this.state = {posts,showMe: false};
   }
+
   onMessageAdd(message) {
    const {posts} = this.state
    const content = Object.assign(message, {
@@ -24,8 +30,8 @@ class ChannelPage extends React.Component {
       } 
      else {
       const newPosts = posts.concat(r.post);
-      this.setState({ posts: newPosts });
-      window.alert("сообщение отправлено");
+      this.setState({ posts: newPosts , showMe:true});
+      setTimeout(() => this.setState({showMe: false}), 3000)
       }
     })
     .catch(e => {
@@ -38,11 +44,12 @@ class ChannelPage extends React.Component {
     const {channels, bots} = INITIAL_DATA
     const {channelId} = this.props;
     const channel = channels.find(c => c.id === channelId)
-    const {posts} = this.state
+    const {posts, showMe} = this.state
     const filteredPosts = this.state.posts.filter(p => p.channel_id === channelId)
     const byDate = (b, a) => a.sentDate - b.sentDate;
     const {title} = channel
       return <div>
+         {showMe===true? <Alert/>: ""}
         <h2>{title}</h2>
         <MessageForm
         bots={bots}
